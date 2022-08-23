14dcab18a0dc2d124b961a15afe81b9272c10086
piyush7827 <piyushshukla7193@gmail.com>
2022-08-23 16:53:47 +0530

    test

---
+++ index.js
@@ -0,0 +1,50 @@
+function Add(){
+    var num1= prompt("Enter First No.");
+    num1=+num1;
+    var num2= prompt("Enter Second No.");
+    num2=+num2;
+    var result=num1 + num2;
+    alert(result);
+    
+}
+    
+    
+    
+    function Subtract(){
+    var num1= prompt("Enter First No.");
+    num1=+num1;
+    var num2= prompt("Enter Second No.");
+    num2=+num2;
+    var result=num1 - num2;
+    alert(result);
+    }
+    
+    function Multiply(){
+    var num1= prompt("Enter First No.");
+    num1=+num1;
+    var num2= prompt("Enter Second No.");
+    num2=+num2;
+    var result=num1 * num2;
+    alert(result);
+    }
+    function Divide(){
+    var num1= prompt("Enter First No.");
+    num1=+num1;
+    var num2= prompt("Enter Second No.");
+    num2=+num2;
+    var result=num1 / num2;
+    alert(result);
+    }
+    function Percentage(){
+        var num1= prompt("Enter First No.");
+        num1=+num1;
+        var num2= prompt("Enter Second No.");
+        num2=+num2;
+        var result=((num1 / num2)*100);
+        alert(result);
+    }
+    function Squreroot(){
+        const number= prompt("Enter First No.");
+        const result=Math.sqrt(number);
+        alert(result);
+    }
